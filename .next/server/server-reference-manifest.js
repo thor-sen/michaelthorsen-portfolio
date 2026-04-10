4948f76bfa5e25759a18af4aa9db23034f3e63ba
Michael Thorsen <michaelthorsen@Michaels-MacBook-Air.local>
2026-04-10 18:51:22 -0400

    Update site font to EB Garamond and color scheme to warm burgundy palette

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"rmguKcImzIZi3/+XVEIe+1xm9hxFDvLF/F6y2HYjO5k=\"\n}"
--- .next/static/chunks/app_page_tsx_3dac7d6a._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_dist_9459fb3a._.js",
-  "static/chunks/components_29032448._.js"
-],
-    source: "dynamic"
-});
