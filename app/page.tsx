a3ceeca5597393c172f5c8701b1d8a53210924d2
Michael Thorsen <michaelthorsen@Michaels-MacBook-Air.local>
2026-04-10 20:56:56 -0400

    Navy color experiment

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { CursorSpotlight } from "@/components/cursor-spotlight";
 import { Sidebar } from "@/components/sidebar";
 import { AboutSection } from "@/components/sections/about";
 import { ExperienceSection } from "@/components/sections/experience";
@@ -8,7 +7,6 @@ import { Footer } from "@/components/footer";
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-background">
-      <CursorSpotlight />
       <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
         <div className="lg:flex lg:justify-between lg:gap-4">
           <Sidebar />
